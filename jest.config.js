1f1c39a8656201951647929a882424a6126daaf9
Kim Holland <tripleaxis@gmail.com>
2019-08-14 21:39:04 +1000

    chore(tests): Removed coverage threshold 😮

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,5 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.js', '!src/search/basic-engine/index.js', '!src/config/*.js'],
   coverageReporters: ['html', 'text', 'text-summary'],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
-    }
-  },
   testEnvironment: 'node'
 };
